6dbaf0a898f5d4f4a918d84434768424fb395f8e
gonzalonunez <gonzaloblasnunez@gmail.com>
2020-09-11 17:17:27 -0700

    Change to good ol' enums

---
+++ Sources/CommonMarkAttributedString/CommonMark+ComponentListConvertible.swift
@@ -0,0 +1,47 @@
+//
+//  CommonMark+ViewListConvertible.swift
+//  CommonMarkAttributedString
+//
+//  Created by Gonzalo Nunez on 9/11/20.
+//
+
+import CommonMark
+import CoreGraphics
+import Foundation
+
+protocol ComponentListConvertible {
+  func makeComponents(with attributes: [NSAttributedString.Key: Any]) throws -> [CommonMarkComponent]
+}
+
+extension Node: ComponentListConvertible {
+  
+  func makeComponents(with attributes: [NSAttributedString.Key: Any]) throws -> [CommonMarkComponent] {
+    switch self {
+    case let image as Image:
+      guard let urlString = image.urlString, let url = URL(string: urlString) else {
+        return []
+      }
+      return [.url(url)]
+      
+    case let container as ContainerOfBlocks:
+      guard !container.children.contains(where: { $0 is HTMLBlock }) else {
+        let html = try Document(container.description).render(format: .html)
+        let htmlString = try NSAttributedString(html: html, attributes: attributes) ?? NSAttributedString()
+        return [.string(htmlString)]
+      }
+      return try container.children.flatMap { try $0.makeComponents(with: attributes) }
+      
+    case let container as ContainerOfInlineElements:
+      guard !container.children.contains(where: { $0 is RawHTML }) else {
+        let html = try Document(container.description).render(format: .html)
+        let htmlString = try NSAttributedString(html: html, attributes: attributes) ?? NSAttributedString()
+        return [.string(htmlString)]
+      }
+      return try container.children.flatMap { try $0.makeComponents(with: attributes) }
+      
+    default:
+      let attributedString = try self.attributedString(attributes: attributes, attachments: [:])
+      return [.string(attributedString)]
+    }
+  }
+}
---
+++ Sources/CommonMarkAttributedString/CommonMarkComponentList.swift
@@ -0,0 +1,26 @@
+//
+//  ViewListConvertible.swift
+//  CommonMarkAttributedString
+//
+//  Created by Gonzalo Nunez on 9/11/20.
+//
+import CommonMark
+import Foundation
+
+public enum CommonMarkComponent {
+  case string(NSAttributedString)
+  case url(URL)
+}
+
+public final class CommonMarkComponentList {
+  
+  public init(
+    commonmark: String,
+    attributes: [NSAttributedString.Key: Any]? = nil) throws
+  {
+    let document = try CommonMark.Document(commonmark, options: [])
+    components = try document.makeComponents(with: attributes ?? [:])
+  }
+  
+  public let components: [CommonMarkComponent]
+}
